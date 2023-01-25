942ec474ce64b531b21bde6512df34144a3a0077
prxvatescrxpts <66436418+prxvatescrxpts@users.noreply.github.com>
2023-01-25 02:28:15 +0200

    Add files via upload

---
+++ audioc/audioc.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <Windows.h>
+#include <mmdeviceapi.h>
+#include <endpointvolume.h>
+#include <initguid.h>
+#include <propkey.h>
+#include <functiondiscoverykeys_devpkey.h>
+#include <functional>
+
+std::function<void()> escapeKeyCallback;
+HHOOK hHook = NULL;
+
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    if (nCode == HC_ACTION)
+    {
+        KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
+        if (wParam == WM_KEYDOWN)
+        {
+            if (p->vkCode == VK_NUMPAD0)
+            {
+                if (escapeKeyCallback)
+                {
+                    escapeKeyCallback();
+                }
+            }
+        }
+    }
+    return CallNextHookEx(hHook, nCode, wParam, lParam);
+}
+
+void InitializeHook()
+{
+    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, NULL, 0);
+    if (hHook == NULL)
+    {
+        std::cout << "Failed to set keyboard hook" << std::endl;
+    }
+}
+
+template<typename T>
+void SetEscapeKeyCallback(T* object, void(T::* callback)())
+{
+    escapeKeyCallback = std::bind(callback, object);
+}
+class AudioKilla {
+public:
+    void KillDevice();
+    void GetDevices();
+
+    int targetDeviceID = NULL;
+    HRESULT device_hr;
+    HRESULT device_name;
+    LPWSTR pwszID = NULL;
+    PROPVARIANT varName;
+    BOOL bMute;
+
+    IMMDeviceEnumerator* pEnumerator = NULL;
+    IMMDeviceCollection* pCollection = NULL;
+
+    IAudioEndpointVolume* pEndpointVolume = NULL;
+    IPropertyStore* pProps = NULL;
+    IMMDevice* pDevice = NULL;
+
+    AudioKilla() {
+        if (FAILED(CoInitializeEx(NULL, COINIT_MULTITHREADED))) {}
+        device_hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL,
+            __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator);
+    }
+    
+    void setTargetDevice(int ID) {
+        targetDeviceID = ID;
+    }
+
+    void getDevices() {
+        pEnumerator->EnumAudioEndpoints(eCapture, DEVICE_STATE_ACTIVE, &pCollection);
+        UINT count;
+        pCollection->GetCount(&count);
+
+        for (UINT i = 0; i < count; i++) {
+            IMMDevice* pDevice = NULL;
+            pCollection->Item(i, &pDevice);
+            pDevice->GetId(&pwszID);
+
+            device_name = pDevice->OpenPropertyStore(STGM_READ, &pProps);
+            PropVariantInit(&varName);
+            device_name = pProps->GetValue(PKEY_Device_FriendlyName, &varName);
+
+            wprintf(L"ID %d - %s\n", i, varName.pwszVal);
+
+            PropVariantClear(&varName);
+            pDevice->Release();
+        }
+    }
+
+    void killDevice(int deviceID) {
+        if (deviceID == NULL) {
+            printf("No ID provided.");
+            return;
+        }
+
+        device_hr = pEnumerator->EnumAudioEndpoints(eCapture, DEVICE_STATE_ACTIVE, &pCollection);
+
+        UINT count;
+        device_hr = pCollection->GetCount(&count);
+
+        for (UINT i = 0; i < count; i++) {
+            IMMDevice* pDevice = NULL;
+            device_hr = pCollection->Item(i, &pDevice);
+            device_hr = pDevice->GetId(&pwszID);
+            device_hr = pEnumerator->GetDevice(pwszID, &pDevice);
+            device_hr = pDevice->Activate(__uuidof(IAudioEndpointVolume),
+                CLSCTX_ALL, NULL, (void**)&pEndpointVolume);
+            device_hr = pEndpointVolume->GetMute(&bMute);
+
+            device_name = pDevice->OpenPropertyStore(STGM_READ, &pProps);
+            PropVariantInit(&varName);
+            device_name = pProps->GetValue(PKEY_Device_FriendlyName, &varName);
+
+            if (i == deviceID) {
+                if (!bMute) {
+                    device_hr = pEndpointVolume->SetMute(TRUE, NULL);
+                    wprintf(L"Muted device %s (ID %d)\n", varName.pwszVal, i);
+                    break;
+                }
+                else {
+                    device_hr = pEndpointVolume->SetMute(FALSE, NULL);
+                    wprintf(L"Unmuted device %s (ID %d)\n", varName.pwszVal, i);
+                    break;
+                }
+            }
+
+            PropVariantClear(&varName);
+            pDevice->Release();
+        }
+    }
+};
+
+void AudioKilla::KillDevice() {
+    device_hr = pEnumerator->EnumAudioEndpoints(eCapture, DEVICE_STATE_ACTIVE, &pCollection);
+
+    UINT count;
+    device_hr = pCollection->GetCount(&count);
+
+    for (UINT i = 0; i < count; i++) {
+        IMMDevice* pDevice = NULL;
+        device_hr = pCollection->Item(i, &pDevice);
+        device_hr = pDevice->GetId(&pwszID);
+        device_hr = pEnumerator->GetDevice(pwszID, &pDevice);
+        device_hr = pDevice->Activate(__uuidof(IAudioEndpointVolume),
+            CLSCTX_ALL, NULL, (void**)&pEndpointVolume);
+        device_hr = pEndpointVolume->GetMute(&bMute);
+
+        device_name = pDevice->OpenPropertyStore(STGM_READ, &pProps);
+        PropVariantInit(&varName);
+        device_name = pProps->GetValue(PKEY_Device_FriendlyName, &varName);
+
+        if (i == targetDeviceID) {
+            if (!bMute) {
+                device_hr = pEndpointVolume->SetMute(TRUE, NULL);
+                wprintf(L"Muted device %s (ID %d)\n", varName.pwszVal, i);
+                break;
+            }
+            else {
+                device_hr = pEndpointVolume->SetMute(FALSE, NULL);
+                wprintf(L"Unmuted device %s (ID %d)\n", varName.pwszVal, i);
+                break;
+            }
+        }
+
+        PropVariantClear(&varName);
+        pDevice->Release();
+    }
+}
+
+void menu() {
+menu:
+    system("cls");
+    printf("1. Set target device\n");
+    printf("2. Listen\n");
+}
+
+int main() {
+    int choice;
+    int target;
+
+    AudioKilla auk;
+    MSG msg;
+
+    InitializeHook();
+    SetEscapeKeyCallback(&auk, &AudioKilla::KillDevice);
+
+    do {
+        menu();
+        printf("\n> ");
+        scanf_s("%i", &choice);
+        switch (choice) {
+        case 1:
+            system("cls");
+            printf("Currently running devices:\n");
+            auk.getDevices();
+            printf("\nTarget ID: ");
+            scanf_s("%i", &target);
+            auk.setTargetDevice(target);
+            break;
+        case 2:
+            while (GetMessage(&msg, NULL, 0, 0))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+            UnhookWindowsHookEx(hHook);
+            break;
+        case 0:
+            system("pause");
+        default: printf("WARN. Incorrect option.\n");
+        }
+    } while (choice != 0);
+    return 0;
+}
